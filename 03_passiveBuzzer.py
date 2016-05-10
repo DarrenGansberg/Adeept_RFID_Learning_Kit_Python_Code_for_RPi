5e32b5cb59d72c743427113cee3d34bb0f3d8469
Darren Gansberg <darmath.mxo@hotmail.com>
2016-05-10 22:00:50 +1000

    Add setup() and loop() functions
    The setup() function is added and script initialisation code refactored
    to setup().
    
    The loop() function is also added that implements the "main" loop for this
    script. The logic that changes the frequency of the buzzer at defined
    intervals has been moved into the loop() function.
    
    Finally, the script is restructured such that setup() and loop() functions
    are only called by the script if it is the main program being executed
    by the python interpreter.

--- 03_passiveBuzzer.py
+++ 03_passiveBuzzer.py
@@ -4,14 +4,12 @@ import time
 
 BZRPin = 12
 
-GPIO.setmode(GPIO.BOARD)       # Numbers pins by physical location
-GPIO.setup(BZRPin, GPIO.OUT)   # Set pin mode as output
-GPIO.output(BZRPin, GPIO.LOW)
+def setup():
+	GPIO.setmode(GPIO.BOARD)       # Numbers pins by physical location
+	GPIO.setup(BZRPin, GPIO.OUT)   # Set pin mode as output
+	GPIO.output(BZRPin, GPIO.LOW)
 
-p = GPIO.PWM(BZRPin, 50) # init frequency: 50HZ
-p.start(50)  # Duty cycle: 50%
-
-try:
+def loop():	
 	while True:
 		for f in range(100, 2000, 100):
 			p.ChangeFrequency(f)
@@ -19,6 +17,14 @@ try:
 		for f in range(2000, 100, -100):
 			p.ChangeFrequency(f)
 			time.sleep(0.2)
-except KeyboardInterrupt:
-	p.stop()
-	GPIO.cleanup()
+	
+if __name__ == '__main__': #Program starts here
+	print 'Press Ctrl+C to end program' 
+	setup()
+	p = GPIO.PWM(BZRPin, 50) #init frequency: 50Hz
+	p.start(50)
+	try:
+		loop()
+	except KeyboardInterrupt:
+		p.stop()
+		GPIO.cleanup()
