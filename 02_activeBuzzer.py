dafedeb0b7ace43e9d45b97ddd5eb66a281f5903
Darren Gansberg <darmath.mxo@hotmail.com>
2016-05-10 20:54:15 +1000

    Fix incorrect pin setup and usage.
    Prior to this change the script setup, and used, pin 11 to turn the buzzer on and off.
    
    The circuit when constructed according to the documentation should utilise pin 12.
    
    Additionally, the script is altered to ensure that the pin is setup off, so the buzzer is off, then the pin is switched on and off in the main loop.

--- 02_activeBuzzer.py
+++ 02_activeBuzzer.py
@@ -2,18 +2,18 @@
 import RPi.GPIO as GPIO
 import time
 
-BeepPin = 11    # pin11
+BeepPin = 12    # pin12
 
 def setup():
 	GPIO.setmode(GPIO.BOARD)        # Numbers pins by physical location
 	GPIO.setup(BeepPin, GPIO.OUT)   # Set pin mode as output
-	GPIO.output(BeepPin, GPIO.HIGH) # Set pin to high(+3.3V) to off the beep
+	GPIO.output(BeepPin, GPIO.LOW) # Set pin to high(+3.3V) to off the beep
 
 def loop():
 	while True:
-		GPIO.output(BeepPin, GPIO.LOW)
-		time.sleep(0.1)
 		GPIO.output(BeepPin, GPIO.HIGH)
+		time.sleep(0.1)
+		GPIO.output(BeepPin, GPIO.LOW)
 		time.sleep(0.1)
 
 def destroy():
