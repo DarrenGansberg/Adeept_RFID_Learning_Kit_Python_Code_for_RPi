26f655af949bc3d5c119caeb9a1166861b649fb2
Darren Gansberg <darmath.mxo@hotmail.com>
2016-09-15 08:52:42 +1000

    Add description of L9110 class and apply BSD 2-clause licence to file.

--- L9110MotorController.py
+++ L9110MotorController.py
@@ -1,4 +1,46 @@
 #!/usr/bin/env python
+
+#########################################
+# L9110 Motor Controller Driver
+#
+# Description: 
+# The L9110MotorController class defined
+# in this file can be used to control 
+# a motor attached to a L9110 motor drive
+# controller IC on a Raspberry Pi (RPi). 
+#
+# To use create instance of the class passing
+# the input pin A and B connected from RPi 
+# to chip, and start speed (should be 0.0 to 100.0) 
+# a step value for speed changes (between 0.0 and 100.0)
+# and the initial direction for the motor (0 = fwd, 1 = backwards)
+#
+#
+# Copyright (C) 2016, Darren Gansberg
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without 
+# modification, are permitted provided that the folowing conditions are met:
+#
+# 1. Redistribution of source code must retain the above copyright notice, this 
+# list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice, this 
+# list of conditions and the following disclaimer in the documentation
+# and/or other materals provided with the distribution. 
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTIONS "AS IS" AND 
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTIONS BE LIABILE FOR ANY DIRECT,
+# INDIRECT, INCIDENTAL, SPECIAL OR EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+# CONTRACT, STRICT LIABILITY, OR TORS (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+
 import RPi.GPIO as GPIO
 import time
 
