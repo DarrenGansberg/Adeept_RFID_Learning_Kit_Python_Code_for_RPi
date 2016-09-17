4e7936309bfab2d966b8e2463671b185d858265e
Darren Gansberg <darmath.mxo@hotmail.com>
2016-09-17 10:43:04 +1000

    Correct naming of destroy functioning and improve efficiency.
    The implementation of getResult() is changed to improve efficiency.
    The previous implementation wasted half of the reads of the joystick,
    by reading the x-axis then returning if it was left, and if it wasn't
    reading the x-axis again. The implementation now reads the x-axis
    and return either left or right. If joystick wasn't moved in the left
    or right direction, the y-axis is read in a similar manner returning
    up/down. Additionally, a short sleep period is added, to slow down
    reading joystick movement to prevent cpu saturation in polling for
    joystick movement.
    
    Finally, button press detection is changed to using interrupt detection
    to improve efficicency.

--- 25_joystick.py
+++ 25_joystick.py
@@ -6,39 +6,57 @@ import time
 
 btn = 15	# Define button pin
 
+
 def setup():
 	ADC0832.setup()				# Setup ADC0832
 	GPIO.setmode(GPIO.BOARD)	# Numbers GPIOs by physical location
 	GPIO.setup(btn, GPIO.IN, pull_up_down=GPIO.PUD_UP)	# Setup button pin as input an pull it up
+	GPIO.add_event_detect(btn, GPIO.FALLING, callback=button_pressed, bouncetime=100)
 	global state
 	state = ['up', 'down', 'left', 'right']	
 
+def button_pressed(channel):
+	print("button pressed")
+	
 def getResult():	#get joystick result
-	if ADC0832.getResult1() == 0:
-		return 1		#up
-	if ADC0832.getResult1() == 255:
-		return 2		#down
+	x_axis = 0
+	y_axis = 1
+	x = ADC0832.getResult(x_axis)	
+	if x == 0:
+		return 4
+	elif x == 255: 
+		return 3
+	y = ADC0832.getResult(y_axis)
+	if y == 0:
+		return 1
+	elif y == 255:
+		return 2
 
-	if ADC0832.getResult() == 0:
-		return 3		#left
-	if ADC0832.getResult() == 255:
-		return 4		#right
+	#if ADC0832.getResult(1) == 0:
+	#	return 1		#up
+	#if ADC0832.getResult(1) == 255:
+	#	return 2		#down
 
-	if GPIO.input(btn) == 1:
-		print 'Button is pressed!'		# Button pressed
+	#if ADC0832.getResult(0) == 0:
+	#	return 3		#left
+	#if ADC0832.getResult(0) == 255:
+	#	return 4		#right
+
+	#if GPIO.input(btn) == 1:
+	#	print 'Button is pressed!'		# Button pressed
 
 def loop():
 	while True:
 		tmp = getResult()
 		if tmp != None:
 			print state[tmp - 1]
-
-def destory():
+		time.sleep(0.1)
+def destroy():
 	GPIO.cleanup()				# Release resource
 
 if __name__ == '__main__':		# Program start from here
-	setup()
 	try:
+		setup()
 		loop()
 	except KeyboardInterrupt:  	# When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
-		destory()
+		destroy()
